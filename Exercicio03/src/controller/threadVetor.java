64031eab9a8286bd9818ec812b147f851927c707
Lucas <lucaswar05@gmail.com>
2024-09-03 23:04:06 -0300

    Exe 03 - Finalizado

--- Exercicio03/src/controller/threadVetor.java
+++ Exercicio03/src/controller/threadVetor.java
@@ -1,5 +1,31 @@
 package controller;
+public class threadVetor extends Thread{
+    private int num;
+    private int[] vet;
+    // Construtor:
+    public threadVetor(int num, int[] vet){
+        this.num = num;
+        this.vet = vet;
+    }
+    // Método de calcTempo
+    private void tempoVet(int[] vet, int num) {
+        double tIni, tFim, tTotal;
+        if((num % 2) == 0) { // Número par
+            tIni = System.nanoTime();
+            for (int i = 0; i < vet.length; i++) {}
+            tFim = System.nanoTime();
+        } else { // Ímpar
+            tIni = System.nanoTime();
+            for (@SuppressWarnings("unused") int i : vet){}
+            tFim = System.nanoTime();
+        }
+        tTotal = tFim - tIni;
+        tTotal = tTotal / Math.pow(10, 9);
+        System.out.println("Tempo de execução: " + tTotal);
+    }
 
-public class threadVetor {
-
+    @Override
+    public void run() {
+        tempoVet(vet,num);
+    }
 }
--- Exercicio03/src/view/classePrincipal.java
+++ Exercicio03/src/view/classePrincipal.java
@@ -1,7 +1,19 @@
 package view;
 import controller.threadVetor;
+import java.util.Random;
 public class classePrincipal {
     public static void main(String[] args) {
-        
+        // Definir variáveis:
+        Random ran = new Random();
+        int[] vet = new int[1000];
+        // Gerar números aleatórios (diferente do Exe 02, com nextInt):
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = ran.nextInt(100);            
+        }
+        for (int j = 1; j < 3; j++) {
+            threadVetor t = new threadVetor(j, vet);
+            t.run();
+        }
+
     }
 }
