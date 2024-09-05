f002a5d1eddf988bf3d536e643a63ae0a520a834
Lucas <lucaswar05@gmail.com>
2024-09-04 21:19:46 -0300

    Exe 04 - Semi-Finalizado

--- Exercicio04/src/controller/ClasseMetodos.java
+++ Exercicio04/src/controller/ClasseMetodos.java
@@ -1,6 +1,15 @@
 package controller;
 import java.util.Random;
 public class ClasseMetodos {
+    private int[] rank = new int[5];
+    /*
+     * Podium matriz?
+     * o valor de linha [0] vai ser usado de flag para travar o lugar no podium para evitar
+     * que os sapos que cheguem depois e seus valores sejam maiores que os anteriores
+     * não substituam os que chegaram antes sendo então:
+     * valor de linha [0] == 0 -> disponível.
+     * valor de linha [0] == 1 -> indisponível.
+     */
     Random rand = new Random();
     // gerar os pulos e verificar:
     public int salto(int sapo, int index){
@@ -11,6 +20,11 @@ public class ClasseMetodos {
             System.out.println("O Sapo "+ (index+1) + " pulou: " + num + "m");
             if(sapo > 19){
                 System.out.println("Sapo " + (index+1) + " chegou!");
+                for (int i = 0; i < rank.length; i++) {
+                    if (rank[i] == 0) {
+                        rank[i] = index + 1;
+                    }                    
+                }
             }
         }
         return sapo;
@@ -21,6 +35,12 @@ public class ClasseMetodos {
         num = rand.nextInt(9) + 1;
         return num;
     }
-    // Classificar posição dos sapos:
+    /* Classificar posição dos sapos:
+    public void classifica(){
+        for (int i = 0; i < rank.length; i++) {
+            System.out.println(rank[i]);
+        }
+    }
+    */
     
 }
--- Exercicio04/src/view/Main.java
+++ Exercicio04/src/view/Main.java
@@ -4,14 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Definir variáveis:
         int sapo = 0;
-        /*
-         * Por que Podium virou uma matriz?
-         * o valor de linha [0] vai ser usado de flag para travar o lugar no podium para evitar
-         * que os sapos que cheguem depois e seus valores sejam maiores que os anteriores
-         * não substituam os que chegaram antes sendo então:
-         * valor de linha [0] == 0 -> disponível.
-         * valor de linha [0] == 1 -> indisponível.
-         */
         // envia os sapos para threads unicas:
         for (int i = 0; i < 5; i++) {
             ClasseThreads t = new ClasseThreads(sapo, i);
